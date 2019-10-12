adb17aff5cbbc25a0de90b9d344ebcb34acacf9c
zequn <jzequn@outlook.com>
2019-10-13 03:42:22 +1300

    local  chagnes

--- readme.md
+++ readme.md
@@ -1,7 +1,12 @@
-# related links
+# MQTT Broker with ionic 4 and angular 8 frameworks
+
+## Related links
 
 1.[Eclipse Paho - MQTT and MQTT-SN software](https://www.eclipse.org/paho/clients/js/)
 
 2.[paho.mqtt.javascript/paho-mqtt.js at master · eclipse/paho.mqtt.javascript](https://github.com/eclipse/paho.mqtt.javascript/blob/master/src/paho-mqtt.js)
 
 3.[HIVEMQ broker](http://www.hivemq.com/demos/websocket-client/)
+
+
+
---
+++ test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# mkdir test
+# touch "test/test.txt"
+# echo "hello zequn" >> "test/test.txt"
+# echo "created test/test.txt"
+
+# curl https://jsonplaceholder.typicode.com/posts/1 >> "test/test.txt"
+# echo "get post done"
+
+
+# curl https://jsonplaceholder.typicode.com/posts/2 >> "test/test.txt"
+# echo "get post done"
+
+
+
+
+# dialog --title text --msgbox "This is a test" 10 20
+#!/bin/bash
+#download latest cURL file automatically
+
+curl -s -o /home/tiandi/curl
+http://curl.haxx.se/download/curl
